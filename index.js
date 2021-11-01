13c4e48896be475a2dd8356275475fcde47ac34e
pengtaoa <1564328778@qq.com>
2021-11-01 16:20:09 +0800

    ::ambulance: feat: add workflow

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { Octokit } = require("octokit");
-const octokit = new Octokit({ auth: `personal-access-token123` });
+const octokit = new Octokit({
+  auth: `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`,
+});
 octokit.rest.issues.create({
-  owner: "octocat",
-  repo: "hello-world",
-  title: "Hello world from " + slug,
+  owner: "pengtaoa",
+  repo: "create-issues-action",
+  title: "每日任务",
+  body: "今天学习javascript 原型链",
 });
